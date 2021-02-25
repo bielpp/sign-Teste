a9d75312d5fbda31e57cbcb77077a27459e78fbc
bielteste <70447319+bielteste@users.noreply.github.com>
2021-02-25 13:56:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+// app.use(cors())
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.json())
 // require('./controllers/authController')(app)
-require('./controllers/root')(app)
+// require('./controllers/root')(app)
 
 
-
+app.get('/', (req, res) => {
+  res.send({ok:'ok'})
+})
 
 app.listen(process.env.PORT || 8080)
