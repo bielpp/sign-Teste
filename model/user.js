961f0acad8adc74a036003826415394983377394
bielteste <70447319+bielteste@users.noreply.github.com>
2021-02-25 12:49:39 -0300

    Create user.js

---
+++ model/user.js
@@ -0,0 +1,37 @@
+const mongoose = require('../database/index')
+const bcrypt = require('bcryptjs')
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        require: true
+    },
+    email:{
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    },
+    querys: {
+        type: Array,
+        lowercase: true
+    },
+    CreatedAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+userSchema.pre('save', async function(next) {
+    const hash = await bcrypt.hash(this.password, 10)
+    this.password = hash
+    next()
+})
+
+const user = mongoose.model('user', userSchema)
+module.exports = user
