0c558f0ad089da2ee481d34d4d455e603095f431
bielteste <70447319+bielteste@users.noreply.github.com>
2021-02-25 12:45:05 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+require('./controllers/authController')(app)
+require('./controllers/root')(app)
+
+
+
+
+app.listen(process.env.PORT || 8080, console.log('iniciando server...'))
